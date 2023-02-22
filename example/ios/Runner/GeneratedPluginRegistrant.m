c377432213b133d1c9919847607a81f88f06078c
James <646724452@qq.com>
2023-02-23 02:56:09 +0800

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_plugin_engagelab/FlutterPluginEngagelabPlugin.h>)
+#import <flutter_plugin_engagelab/FlutterPluginEngagelabPlugin.h>
+#else
+@import flutter_plugin_engagelab;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterPluginEngagelabPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterPluginEngagelabPlugin"]];
+}
+
+@end
